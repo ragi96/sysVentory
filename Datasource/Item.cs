e8faa4544cd60759025ab32adf04fd8fdfb734fb
LAPTOP-HTGNJBPQ\ragi <ragi96@protonmail.ch>
2020-08-17 17:14:28 +0200

    basic start

---
+++ Datasource/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Datasource {
+    public class Item {
+        public Item(string itemType, List<ItemProperty> properties) {
+            this.ItemType = itemType;
+            this.Properties = properties;
+        }
+
+        public string ItemType { get; }
+        public List<ItemProperty> Properties { get; }
+    }
+}
---
+++ Datasource/ItemProperty.cs
@@ -0,0 +1,11 @@
+﻿namespace Datasource {
+    public class ItemProperty {
+        public ItemProperty(string name, object value) {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public string Name { get; }
+        public object Value { get; }
+    }
+}
